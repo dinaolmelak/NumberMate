0dda7559e5a726883d5de7f168c176bced0a8b66
= <=>
2020-07-04 02:05:31 -0500

    layout completed

---
+++ NumberMate/FormViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FormViewController.swift
+//  NumberMate
+//
+//  Created by Dinaol Melak on 7/3/20.
+//  Copyright © 2020 Dinaol Melak. All rights reserved.
+//
+
+import UIKit
+
+class FormViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
