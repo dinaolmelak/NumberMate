705182c2516061e084d58c3db20b77fe2f1c7d80
dinaolmelak <dinaolmelak@gmail.com>
2020-10-10 23:34:09 -0500

    Added FSCalendar

--- NumberMate/CalendarView/MonthViewController.swift
+++ NumberMate/CalendarView/MonthViewController.swift
@@ -7,13 +7,15 @@
 //
 
 import UIKit
+import FSCalendar
 
 class MonthViewController: UIViewController{
    
-        
+    
+    @IBOutlet weak var calendar: FSCalendar!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
